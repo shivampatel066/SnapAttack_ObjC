b52556ad4af6f6c5e3b0128969b5761a2e3a92b7
Shivam Patel <shivampatel066@gmail.com>
2019-12-08 18:59:26 +0530

    SnapAttack_Objc
    Its a Reaction Based Game, in which two cards are generated randomly on the screen, you have to click as soon as both the cards matches and points are increased or decreased based on the correctness of your clicks.

--- SnapAttack_ObjC/ViewController.h
+++ SnapAttack_ObjC/ViewController.h
@@ -8,7 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    NSTimer *timer;
+    NSTimer *imageTimer;
+    
+    int gameMode;
+    int scoreInt;
+    int timerInt;
+    
+}
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+- (IBAction)startGame:(id)sender;
 
 
 @end
--- SnapAttack_ObjC/ViewController.m
+++ SnapAttack_ObjC/ViewController.m
@@ -16,8 +16,116 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    timerInt = 20;
+    scoreInt = 0;
+    gameMode = 0;
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"Time: %i",timerInt];
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i",scoreInt];
+
 }
 
+- (IBAction)startGame:(id)sender {
+    
+    if (timerInt == 20) {
+        
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:true];
+        imageTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(cardUpdate) userInfo:nil repeats:YES];
+        
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.25;
+        
+        [self.startButton setTitle:@"Snap" forState:UIControlStateNormal];
+        
+        
+    }
+    
+    if (gameMode == 1) {
+        
+        if ([self.imageView1.image isEqual:self.imageView2.image]) {
+            
+            scoreInt++;
+            self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i",scoreInt];
+        } else {
+            scoreInt--;
+            self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i",scoreInt];
+        }
+        
+    }
+    if (timerInt == 0) {
+        
+        timerInt = 20;
+        scoreInt = 0;
+        gameMode = 0;
+        
+        self.timeLabel.text = [NSString stringWithFormat:@"Time: %i",timerInt];
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i",scoreInt];
+        
+        self.imageView1.image = [UIImage imageNamed:@"blue_cover"];
+        self.imageView2.image = [UIImage imageNamed:@"blue_cover"];
+        
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    
+}
+    
+- (void)updateTimer {
+    
+    timerInt--;
+    self.timeLabel.text = [NSString stringWithFormat:@"Time: %i",timerInt];
+    
+    gameMode = 1;
+    
+    self.startButton.enabled = YES;
+    self.startButton.alpha = 1.0;
+    
+    
+    if (timerInt == 0) {
+        
+        
+        [timer invalidate];
+        [imageTimer invalidate];
+        
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.25;
+        
+        [self.startButton setTitle:@"Restart" forState:UIControlStateNormal];
+        
+        [self performSelector:@selector(restartTrigger) withObject:nil afterDelay:4.0];
+        
+        
+        
+    }
+    
+}
+-(void)cardUpdate {
+    
+    NSArray *cardList = @[@"ace_of_hearts",
+                        @"2_of_hearts",
+                        @"3_of_hearts",
+                        @"4_of_hearts",
+                        @"5_of_hearts",
+                        @"6_of_hearts",
+                        @"7_of_hearts",
+                        @"8_of_hearts",
+                        @"9_of_hearts",
+                        @"10_of_hearts"];
+    
+    int randomCard1 = arc4random() % cardList.count;
+    self.imageView1.image = [UIImage imageNamed:cardList[randomCard1]];
+    
+    int randomCard2 = arc4random() % cardList.count;
+    self.imageView2.image = [UIImage imageNamed:cardList[randomCard2]];
+    
+}
+-(void)restartTrigger {
+    
+    self.startButton.enabled = YES;
+    self.startButton.alpha = 1.0;
+    
+    gameMode = 0;
+    
+}
 
 @end
